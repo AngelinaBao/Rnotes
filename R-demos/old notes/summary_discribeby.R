7a683e5593d3dedfb5822c3563460116d75d9bee
AngelinaBao <2276913347@qq.com>
2018-12-23 22:38:38 +0800

    reverse

---
+++ R-demos/old notes/summary_discribeby.R
@@ -0,0 +1,17 @@
+-myfun <- function(x, na.omit = FALSE){
+  if(na.omit)
+    x <- x[!is.na(x)]
+  m <- mean(x)
+  n <- length(x)
+  s <- sd(x)
+  skew <- sum((x-m)^3/s^3)/n
+  kurt <- sum((x-m)^4/s^4)/n-3
+  return(c(n = n, mean = m, sd = s, skew = skew, kurt = kurt))
+}
+install.packages("doBy")
+library(doBy)
+summaryBy(mpg+hp+wt ~ am, data = mtcars, FUN = myfun)
+library(psych)
+car <- c("mpg","hp","wt")
+describeBy(mtcars[car], list(am = mtcars$am))
+#summaryBy??????????????????,???describeBy?????????,?????????????????????
